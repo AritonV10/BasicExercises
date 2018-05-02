085ad88460248286ef6f05c7ccb5ca094b2f9b56
Ariton Vio <ariton.vio@gmail.com>
2018-05-02 22:03:44 +0300

    Update InterschimbareADouaCifre.cpp

--- UVT/InterschimbareADouaCifre.cpp
+++ UVT/InterschimbareADouaCifre.cpp
@@ -34,13 +34,12 @@ int swap_num(int number, int a, int b){
 
     int closer_to_end = cif_at(number, b), closer_to_start = cif_at(number, a);
     int _mirror = 0, _count = 1;
-    while(number > 0){
-           
+    while(number > 0){     
         int last = number % 10;
         if(_count == b) 
             _mirror = _mirror*10+closer_to_start;
-        else if
-            (_count == a) _mirror = _mirror*10+closer_to_end;
+        else if(_count == a) 
+            _mirror = _mirror*10+closer_to_end;
         else 
              _mirror = _mirror*10+last;
      
