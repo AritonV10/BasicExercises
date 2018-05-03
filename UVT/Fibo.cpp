d352ece3db026513983bbf93eff4a23189d07d62
Ariton Vio <ariton.vio@gmail.com>
2018-05-04 00:18:13 +0300

    Create Fibo.cpp

---
+++ UVT/Fibo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void fib_swap(int& a, int& b, int& c){
+    c = a + b; a = b; b = c;
+}
+bool func(const int* n, const int* _p){
+    int a = 0, b = 1, c = 0;
+    for(int i = 0; i <= *n; i++){
+        fib_swap(a, b, c);
+        if(c == *_p)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    int x = 30, f = 34;
+    int* _p = &x;
+    int* p_ = &f;
+    if(func(_p, p_))
+        std::cout<<"Is part of fibo";
+    else
+        std::cout<<"Not part of fibo";
+}
