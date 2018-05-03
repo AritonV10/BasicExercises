c92aec3a5d1b399eb902a793c079c4355d17707e
Ariton Vio <ariton.vio@gmail.com>
2018-05-03 21:33:17 +0300

    Update PrimeFactors.cpp

--- UVT/PrimeFactors.cpp
+++ UVT/PrimeFactors.cpp
@@ -4,8 +4,8 @@ int last_dig(int n);
 bool isDivisible(int num);
 void easy_fun(int n);
 std::vector<int> v;
-int main(){
 
+int main(){
     easy_fun(55);
     for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++){
         std::cout<<*it << " ";
@@ -28,12 +28,10 @@ void easy_fun(int n){
 
 bool isDivisible(int num){
     for(int i = 2; i < num; i++)
-        if(num % i == 0)
-            return true;
-
+        if(num % i == 0) return true;
+    
     return false;
 }
-
 int last_dig(int n){
     for(int i = 2;;i++)
         if(n % i == 0) return i;
