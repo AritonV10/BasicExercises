1636be52ff1c1547d8877e47b71141c8329a053c
Ariton Vio <ariton.vio@gmail.com>
2018-05-03 17:51:41 +0300

    Create Wilson.cpp

---
+++ UVT/Wilson.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int func(int p){
+    if(p == 0) return 1;
+    return p * func(p-1);
+}
+bool wilson(int n){
+    if((func(n-1)+1)%n==0)
+        return true;
+    else
+        return false;
+}
+int main(){
+
+   if(wilson(11))
+    std::cout<<"PRIME";
+   else
+    std::cout<<"NOT_PRIME";
+
+}
