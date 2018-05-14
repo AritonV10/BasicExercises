51f1ce62dba48113a7dcc2cefb5cce5f08c73c7e
Ariton Vio <ariton.vio@gmail.com>
2018-05-14 19:11:25 +0300

    Create ex_2.cpp

---
+++ ex_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int power(int k, int p){
+    if(k == 0) return p;
+    return power(k-1, p*10);
+}
+
+int dig(int k){
+    int _count = 0;
+    while(k > 0){
+        _count++;
+        k /= 10;
+    }
+    return _count;
+}
+
+double nreal(int n, int k){
+    int s_dig = dig(k), s_z = power(s_dig, 1), x = (n * s_z) + k, x_cif = dig(x);
+
+    return (double) x / s_z;
+}
+
+int main(){}
