998c76097c61b98a5716c0dc79aa593e69ede2e8
Ariton Vio <ariton.vio@gmail.com>
2018-05-14 18:34:51 +0300

    Create ex_1.cpp

---
+++ ex_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int sum_cif(int k){
+    int sum = 0;
+    while(k > 0){
+        sum += k % 10;
+        k /= 10;
+    }
+    return sum;
+}
+int cif(int k, int a){
+    if(k == a) return k;
+    if(k < 10 && k != a) return 0;
+    return cif(sum_cif(k), a);
+}
+bool is_equal(int k, int a){
+    return cif(k, a) == a;
+}
+int func(int a, int k){
+    int nr_apariti = 0;
+    for(int i = a; i <= k; i++){
+        if(is_equal(i, a)) nr_apariti++;
+    }
+    return nr_apariti;
+}
+
+int main(){
+
+    std::cout<<func(2, 100);
+}
+
+
+
+
