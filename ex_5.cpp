6d5de74a0a2b5cc74f7a760337d0ec52d8678903
Ariton Vio <ariton.vio@gmail.com>
2018-05-15 22:37:04 +0300

    Update ex_5.cpp

--- ex_5.cpp
+++ ex_5.cpp
@@ -2,19 +2,20 @@
 #include <iostream>
 
 bool vizitat[50001];
-int diofantic(const unsigned int vec_length, const int arr[], const int a, const int b, const int c){
+int diofantic(int vec_length, int arr[], int a, int b, int c){
     int _pairs = 0;
-    long sum_a, sum_b;
-    for(unsigned int i = 1; i <= vec_length; i++){
+    long long sum_a, sum_b;
+    bool _same = (a == b ? true : false);
+    for(int i = 1; i <= vec_length; i++){
         sum_a = a*(arr[i]*arr[i]);
         if(sum_a > c) break;
-        //else if(vizitat[i]) continue;
-        for(unsigned int j = 1; j <= vec_length; j++){
+        else if(_same){ if(vizitat[i]) continue; }
+        for(int j = 1; j <= vec_length; j++){
             sum_b = b*(arr[j]*arr[j]);
-            //if(vizitat[j]) continue;
-            if(sum_a + sum_b == c && arr[i] != arr[j]) { std::cout<<"(" << arr[i] << ", " << arr[j] << ") \n"; vizitat[j] = true; _pairs += 2; break;}
+            if(_same){ if(vizitat[j]) continue; }
+            if(sum_a + sum_b == c && arr[i] != arr[j]) { if(_same) {vizitat[j] = true; } _pairs++; break;}
             else if(sum_a + sum_b > c) { break; }
-        }
+        }     
     }
     return _pairs;
 }
