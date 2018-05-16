a8cb394021873d97e8863c395e0a24617562453b
Ariton Vio <ariton.vio@gmail.com>
2018-05-16 21:42:23 +0300

    Update ex_8.cpp

--- ex_8.cpp
+++ ex_8.cpp
@@ -2,20 +2,20 @@
 #include <iostream>
 
 bool is_prime(int n){
-    int _p = 1;
+    bool _is_prime = true;
     for(int i = 2; i < n; i++)
-        if(n%i==0) _p=-1;
-    return _p == 1;
+        if(n%i==0) _is_prime=false;
+    return _is_prime;
 }
 int func(const int arr[], int arr_length, int& x, /*private*/ int k){
-    if(arr_length == 1) return k;
-    return func(arr, arr_length - 1, x, k+= (is_prime(arr[arr_length])));
+    if(arr_length == 0) return k;
+    return func(arr, arr_length - 1, x, k+(is_prime(arr[arr_length]) ? arr[arr_length] : 0));
 }
 int func(const int arr[], int arr_length, int& x){
     return x = func(arr, arr_length - 1, x, 0);
 }
 int main(){
-    int x, arr[] = {1, 5, 5, 6, 6, 3, 5, 5};
-    func(arr, 8, x);
+    int x, arr[] = {12, 7, 6, 3, 8, 5};
+    func(arr, 6, x);
     std::cout<<x;
 }
