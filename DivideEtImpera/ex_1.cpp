8361b566e11df04cfd1e98dec6724cb202e24f03
Ariton Vio <ariton.vio@gmail.com>
2018-05-18 18:32:29 +0300

    Update ex_1.cpp

--- DivideEtImpera/ex_1.cpp
+++ DivideEtImpera/ex_1.cpp
@@ -1,7 +1,7 @@
 /*https://www.pbinfo.ro/?pagina=probleme&id=1015*/
 #include <iostream>
 int combine(int first_half_sum, int second_half_sum) { return first_half_sum + second_half_sum; }
-int second_half(int arr[], int st, int d){ // 5, 6, 7, 8 -> 8/(5-1) -> 6 -> [5, 6]
+int second_half(int arr[], int st, int d){ // 5, 6, 7, 8 -> 8-(8/(5-1)) -> 6 -> [5, 6]
     if(st+1 == d) return arr[st] + arr[d];
     else if(st == d) { return arr[st]; }
     return second_half(arr, st, d-(d/((d/2)))) + second_half(arr, st+(d/st), d); // 5, 6, 7 -> 5+(7/4) -> 5+1 -> 6 -> [6, 7]
