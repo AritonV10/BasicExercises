a04aa5e70478b6accb046cd5d71f4d1acfe03706
Ariton Vio <ariton.vio@gmail.com>
2018-06-03 23:24:38 +0300

    Update ex_2.cpp

--- UVT/BT/ex_2.cpp
+++ UVT/BT/ex_2.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <fstream>
 
-//ifstream fin("test.in");
-//ofstream fout("test.out");
-
 void swap_elements(int * v, int a_pos, int b_pos){
     int temp = v[a_pos];
     v[a_pos]=v[b_pos];
     v[b_pos]=temp;
 }
-bool hasMore(int number){ return number==0; }
+bool hasMore(int number){ return number!=0; }
 int findSmallest(int * v, unsigned v_size){
     int _pos = v_size;
     while(v[_pos] < v[_pos-1] && _pos > 1) _pos--;
@@ -32,16 +29,18 @@ void func(int*v, unsigned v_size){
     int _pos;
     do{
          _pos = findSmallest(v, v_size);
-        if(_pos){
+        if(hasMore(_pos)){
+
             int b_num = v[_pos+1], b_cop = b_num, b_pos=_pos+1;
-            for(int i = _pos + 1; i <= v_size; i++)
+            for(int i = _pos + 1; i <= v_size; i++){
                 if(b_num > v[i] && v[i] > v[_pos]) { b_num = v[i]; b_pos = i; }
+            }
             if(b_cop == b_num) swap_elements(v, _pos, _pos+1);
             else swap_elements(v, _pos, b_pos);
             sort_array(v, v_size, _pos+1);
             display_array(v, v_size);
         }
-    }while(_pos);
+    }while(hasMore(_pos));
 }
 auto main() -> int{
    int arr[100];
